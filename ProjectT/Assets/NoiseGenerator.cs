5b1fd2a4f7fdf583be26bb5e33ba8e6dd437cc8f
Philipp Mandler <info@philipp-mandler.com>
2015-05-21 01:42:29 +0200

    first perlin noise

---
+++ ProjectT/Assets/NoiseGenerator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoiseGenerator : MonoBehaviour {
+
+	public int pixWidth;
+	public int pixHeight;
+	public float xOrg;
+	public float yOrg;
+	public float scale = 1.0f;
+
+
+	void Start () {
+		Renderer renderer = GetComponent<Renderer> ();
+		Texture2D noiseTex = new Texture2D (pixWidth, pixHeight);
+		Color[] pix = new Color[noiseTex.width * noiseTex.height];
+		renderer.material.mainTexture = noiseTex;
+
+
+		for(float y = 0; y < noiseTex.height; y++) {
+			for(float x = 0; x < noiseTex.width; x++) {
+				float xCoord = xOrg + x / noiseTex.width * scale;
+				float yCoord = yOrg + y / noiseTex.height * scale;
+
+				float sample = Mathf.PerlinNoise (xCoord, yCoord);
+
+				pix [(int)(y * noiseTex.width + x)] = new Color (sample, sample, sample);
+			}
+		}
+
+		noiseTex.SetPixels (pix);
+		noiseTex.Apply ();
+
+	}
+
+
+	void Update () {
+
+	}
+}
