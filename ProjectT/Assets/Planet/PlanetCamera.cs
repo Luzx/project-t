825ad3c6a8fda1d976901ae32985756ac3c31a15
Philipp Mandler <info@philipp-mandler.com>
2015-05-25 04:29:57 +0200

    Added planet scene

---
+++ ProjectT/Assets/Planet/PlanetCamera.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetCamera : MonoBehaviour {
+
+	public float MoveSpeed = 0.1f;
+	public float zoom = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var movement = new Vector3();
+
+		zoom *= 1.0f + (Input.mouseScrollDelta.y * 0.1f);
+
+		if(Input.GetKey(KeyCode.UpArrow)) {
+			movement += Vector3.up * MoveSpeed;
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			movement += Vector3.left * MoveSpeed;
+		}
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			movement += Vector3.right * MoveSpeed;
+		}
+		if(Input.GetKey(KeyCode.DownArrow)) {
+			movement += Vector3.down * MoveSpeed;
+		}
+
+		// android
+		movement.x += Input.acceleration.x;
+		movement.y += Input.acceleration.y;
+
+		transform.position = transform.position + movement * zoom * Time.deltaTime * 30f;
+
+		GetComponent<Camera> ().orthographicSize = zoom;
+	}
+}
---
+++ ProjectT/Assets/Planet/PlanetFactory.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlanetFactory : MonoBehaviour {
+	
+	
+	public GameObject terrainFactory;
+	public Camera terrainCamera;
+	
+	public bool created = false;
+	public bool createdHighRes = false;
+
+	RenderTexture nirvanaTexture;
+
+	public RenderTexture sdTerrain;
+	public RenderTexture hdTerrain;
+	
+	// Use this for initialization
+	void Start () {
+		
+		nirvanaTexture = new RenderTexture (1, 1, 24);
+
+		terrainCamera.targetTexture = nirvanaTexture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftControl)) {
+			created = false;
+			createdHighRes = false;
+		}
+	}
+
+	private RenderTexture buildTerrain(int x, int y, RenderTexture renderTexture) {
+
+		//var texRenderer = plane.GetComponent<Renderer> ();
+
+		//texRenderer.material.mainTexture = renderTexture;
+
+		var terrainGen = terrainFactory.GetComponent<TerrainGenerator> ();
+		terrainGen.x = - x * 10 * terrainGen.scale;
+		terrainGen.y = - y * 10 * terrainGen.scale;
+
+		terrainCamera.targetTexture = renderTexture;
+		terrainCamera.enabled = true;
+
+		return renderTexture;
+	}
+
+	
+	void OnPostRender() {
+		terrainCamera.targetTexture = nirvanaTexture;
+
+		if (!created) {
+			buildTerrain (0, 0, sdTerrain);
+			created = true;
+		} else if (!createdHighRes) {
+			buildTerrain (0, 0, hdTerrain);
+			createdHighRes = true;
+		}
+
+	}
+	
+	public void rebuild() {
+		created = false;
+		createdHighRes = false;
+	}
+	
+}
